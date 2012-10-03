776c605e230fc06c86d22a51d62b72ff9038c1a1
LorDClockaN <davor@losinj.com>
2012-10-03 17:03:20 +0200

    Quick Reply: Add option to auto-open QR when new msg comes in
    I know that this isn't exactly how Google wants notificaions to work,
    but most 3rd-party SMS apps have this option, and I set it up to be
    turned off by default.
    
    Patch3: added code to prevent auto-open if the screen is locked
    Patch4: fixed the pref description
    Patch6: fixed a missing bracked from Patch5, added precautions
    to prevent crashed if KeygardManager or TelephonyManager are Null
    Patch7: rebase
    Patch8: added option to allow popup when screen is locked
    
    Change-Id: Ia5c91399c1c054e1f45f6f04a5bb247d264240f8
    
    Conflicts:
    
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -42,6 +42,7 @@ import com.google.android.mms.pdu.PduHeaders;
 import com.google.android.mms.pdu.PduPersister;
 import android.database.sqlite.SqliteWrapper;
 
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -216,7 +217,7 @@ public class MessagingNotification {
      * play the notification sound at a lower volume. Make sure you set this to
      * THREAD_NONE when the UI component that shows the thread is no longer
      * visible to the user (e.g. Activity.onPause(), etc.)
-     * 
+     *
      * @param threadId The ID of the thread that the user is currently viewing.
      *            Pass THREAD_NONE if the user is not viewing a thread, or
      *            THREAD_ALL if the user is viewing the conversation list (note:
@@ -232,7 +233,7 @@ public class MessagingNotification {
      * Checks to see if there are any "unseen" messages or delivery reports.
      * Shows the most recent notification if there is one. Does its work and
      * query in a worker thread.
-     * 
+     *
      * @param context the context to use
      */
     public static void nonBlockingUpdateNewMessageIndicator(final Context context,
@@ -249,7 +250,7 @@ public class MessagingNotification {
     /**
      * Checks to see if there are any "unseen" messages or delivery reports and
      * builds a sorted (by delivery date) list of unread notifications.
-     * 
+     *
      * @param context the context to use
      * @param newMsgThreadId The thread ID of a new message that we're to notify
      *            about; if there's no new message, use THREAD_NONE. If we
@@ -807,7 +808,7 @@ public class MessagingNotification {
     /**
      * updateNotification is *the* main function for building the actual
      * notification handed to the NotificationManager
-     * 
+     *
      * @param context
      * @param isNew if we've got a new message, show the ticker
      * @param uniqueThreadCount
@@ -1043,6 +1044,21 @@ public class MessagingNotification {
                 PendingIntent piText = PendingIntent.getActivity(context, 0, quickReply,
                         PendingIntent.FLAG_UPDATE_CURRENT);
                 noti.addAction(R.drawable.ic_menu_msg_compose_holo_dark, quickText, piText);
+
+                if(MessagingPreferenceActivity.getQRAutoOpenEnabled(context) && mostRecentNotification.mIsSms) {
+                    TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                    KeyguardManager kgMgr = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+
+		    if(MessagingPreferenceActivity.getLockSmsEnabled(context)) {
+			if (tm.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+			    context.startActivity(quickReply);
+			}
+		    } else if(kgMgr == null || !kgMgr.inKeyguardRestrictedInputMode()) {
+                        if(tm == null || tm.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+                            context.startActivity(quickReply);
+                        }
+                    }
+                }
             }
         }
 
@@ -1292,7 +1308,7 @@ public class MessagingNotification {
     /**
      * Query the DB and return the number of undelivered messages (total for
      * both SMS and MMS)
-     * 
+     *
      * @param context The context
      * @param threadIdResult A container to put the result in, according to the
      *            following rules: threadIdResult[0] contains the thread id of
@@ -1400,7 +1416,7 @@ public class MessagingNotification {
 
     /**
      * Get the message ID of the SMS message with the given URI
-     * 
+     *
      * @param context The context
      * @param uri The URI of the SMS message
      * @return The message id
@@ -1424,7 +1440,7 @@ public class MessagingNotification {
 
     /**
      * Get the thread ID of the SMS message with the given URI
-     * 
+     *
      * @param context The context
      * @param uri The URI of the SMS message
      * @return The thread ID, or THREAD_NONE if the URI contains no entries
@@ -1456,7 +1472,7 @@ public class MessagingNotification {
 
     /**
      * Get the thread ID of the MMS message with the given URI
-     * 
+     *
      * @param context The context
      * @param uri The URI of the SMS message
      * @return The thread ID, or THREAD_NONE if the URI contains no entries
